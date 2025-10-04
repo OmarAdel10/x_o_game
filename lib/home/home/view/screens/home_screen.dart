2ccbba4bef6f84d7642d6aabd242c7dd6e3f850e
Omar Adel <156123711+OmarAdel10@users.noreply.github.com>
2025-10-04 08:03:04 +0300

    - increased spaceing between items

--- lib/home/home/view/screens/home_screen.dart
+++ lib/home/home/view/screens/home_screen.dart
@@ -84,7 +84,7 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
 
-              const SizedBox(height: 20),
+              const SizedBox(height: 32),
 
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
